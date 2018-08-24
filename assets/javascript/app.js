3ca91988f062c0e5c3b9e8cec386fa61af4ae640
Ennykay7 <ksperkins.ksp@gmail.com>
2018-08-24 04:56:53 -0700

    everything works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,22 @@
 var questions = [
     "N64 controllers had three places for your hands.",
     "The PS2 console used cartridges.",
-    "The Gameboy Advanced was designed vertically."
+    "The Gameboy Advanced was designed vertically.",
+    "The DS lite has two screens.",
+    "The Xbox console is black and green."
 ];
 var answers = [
     "t",
     "f",
-    "f"
+    "f",
+    "t",
+    "t"
 ];
 var unanswered = 0;
 var correct = 0;
 var wrong = 0;
+var time = 10;
+var intervalId;
 
 function createQuestions() {
     $("#playArea").html("<form id=questionArea></form>")
@@ -29,19 +35,30 @@ function createQuestions() {
     }
 }
 
-function tallyScore(){
-    for(var i = 0; i < questions.length; i++){
-        var radios = $("input[name=inlineRadioOptions" + i +"]");
-        console.log(radios)
-        if (radios[0].checked === false && radios[1].checked === false){
+function tallyScore() {
+    for (var i = 0; i < questions.length; i++) {
+        var radios = $("input[name=inlineRadioOptions" + i + "]");
+        if (radios[0].checked === false && radios[1].checked === false) {
             unanswered++;
-        } else if (radios[0].checked && radios[0].value === answers[i]){
+        } else if (radios[0].checked && radios[0].value === answers[i]) {
             correct++;
-        }else if (radios[1].checked && radios[1].value === answers[i]){
+        } else if (radios[1].checked && radios[1].value === answers[i]) {
             correct++;
         } else {
             wrong++;
         }
+    }
+}
+
+function countDown() {
+    if(time === 0){
+        clearInterval(intervalId);
+        tallyScore();
+        $("#playArea").html("<div>Correct: " + correct + "</div><div>Wrong: " + wrong + "</div><div>Unanswered: " + unanswered + "</div>");
+
+    }else{
+    time--;
+    $("#timer").text(time);
     }
 }
 
@@ -50,10 +67,16 @@ $("#startGame").on("click", function () {
     $("#playArea").append(
         "<button id='finish'>Done</button>"
     )
-    
+
 });
 
-$(document).on("click", "#finish", function(){
+
+$("#startGame").on("click", function () {
+    intervalId = setInterval(countDown, 1000);
+});
+
+$(document).on("click", "#finish", function () {
     tallyScore();
     $("#playArea").html("<div>Correct: " + correct + "</div><div>Wrong: " + wrong + "</div><div>Unanswered: " + unanswered + "</div>");
 });
+
